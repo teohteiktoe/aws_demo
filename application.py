745613b221ed7f8b4b66393be7bd30d3538005c9
Teoh TeikToe <teohteiktoe@gmail.com>
2025-05-19 06:40:19 +0000

    1

--- application.py
+++ application.py
@@ -1,13 +1,18 @@
-from flask import Flask, request, render_template
+from textblob import TextBlob
+from flask import Flask
+
 application = Flask(__name__)
+from flask import render_template,request
 
-@application.route("/", methods=["GET","POST"])
-def index():    
-    if request.method == "POST":        
-        q = request.form.get("q")
-        return(render_template("index.html",result=r))    
+@application.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form.get("text")
+        print(text)
+        r = TextBlob(text).sentiment
+        return(render_template("index.html", result=r))
     else:
-        return(render_template("index.html",result="waiting.........."))
+        return(render_template("index.html", result="waiting"))
 
-if __name__ == "__main__":    
+if __name__=="__main__":
     application.run()
