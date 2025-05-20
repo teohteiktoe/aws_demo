e84bf520bf16edf18c1b23217a7be59f425fbd31
Teoh TeikToe <teohteiktoe@gmail.com>
2025-05-20 08:48:19 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/", methods=["GET","POST"])
 def index():    
-    if request.method == "POST":        
-        r = float(request.form.get("rate"))
-        return(render_template("index.html",result=r*(-50.6)+90.2))    
-    else:
-        return(render_template("index.html",result="waiting.........."))
+    return(render_template("index.html"))
 
 if __name__ == "__main__":    
     application.run()
